5a2dd1c80e225e79862af64c504f3c892740aca9
Souad <osouad@umich.edu>
2022-10-20 16:56:05 -0400

    changed b

--- quraticsolver.java
+++ quraticsolver.java
@@ -4,7 +4,7 @@ public class quraticsolver {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a = 4;
-		int b = 4;
+		int b = 5;
 		int c = 3;
 		if (b*b - 4*a*c <0)
 			System.out.println("This equation " + a + "x^2 + " + b +"x + " + c +" has no solution");
